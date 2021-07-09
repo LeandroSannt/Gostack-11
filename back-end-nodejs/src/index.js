3dbba1d4d512bb00e086d861b0b43219b8a8182e
LeandroSannt <lsn_slim@yahoo.com.br>
2021-07-09 11:38:54 -0300

    iniciando front

--- back-end-nodejs/src/index.js
+++ back-end-nodejs/src/index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid');
 
+const isuuid = require('isuuid');
+
 const app = express()
 
 app.use(express.json())
 
+app.use("/projects/:id",validateProjectId)
+
 
 const projects = []
 
-app.get('/projects',(req,res)=>{
+
+function logRequests(req,res,next){
+    const {method,url} = req
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`
+
+    //console.log(logLabel)
+
+    return next()
+}
+
+function validateProjectId(req,res,next){
+
+    const {id} = req.params
+
+    if(!isuuid(id)){
+        return res.status(400).json({error:"Invalid id"})
+    }
+
+    return next()
+
+}
+
+app.use(logRequests)
+
+app.get('/projects',logRequests,(req,res)=>{
 
     const {title} = req.query
 
