fd11616000aa72737d37479a850497e16d21631e
LeandroSannt <lsn_slim@yahoo.com.br>
2021-09-10 06:12:49 -0300

    teste de autenticação

--- gobarber/gobarber-api/src/modules/users/services/AuthenticateUserService.spec.ts
+++ gobarber/gobarber-api/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -26,4 +26,38 @@ describe('CreateUser', () => {
     expect(response).toHaveProperty('token')
     expect(response.user).toEqual(user)
   })
+
+  it('não pode autenticar usuario q nao existe',async () =>{
+
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository,fakeHashProvider)
+
+    expect(authenticateUser.execute({
+      email:"lsn_slim@yahoo.com.br",
+      password:"123456"
+    })).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('Não pode logar com usuario com a senha errada',async () =>{
+
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(fakeUsersRepository,fakeHashProvider);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository,fakeHashProvider)
+
+    await createUser.execute({
+      name:"leandro",
+      email:"lsn_slim@yahoo.com.br",
+      password:"123456"
+    })
+
+
+    expect(authenticateUser.execute({
+      email:"lsn_slim@yahoo.com.br",
+      password:"rhggerg"
+    })).rejects.toBeInstanceOf(AppError)
+  })
 })
